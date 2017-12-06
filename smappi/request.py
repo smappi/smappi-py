eae1152f314b7c295327e10b6ee48349947c491e
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2017-12-06 14:16:04 +0300

    Removed __getattr__ (cause used new-style classes for py2); Added self for super (py2)

--- smappi/request.py
+++ smappi/request.py
@@ -13,18 +13,15 @@ except ImportError:
 from .exceptions import PositionalArgumentsNotSupported, SmappiServerError
 
 
-class Request:
+class Request(object):
 
     def __init__(self, path, fmt='json'):
         self._fmt = fmt
         self._path = path
 
-    def __getattr__(self, name):
-        return self.__getattribute__(name)
-
     def __getattribute__(self, name):
         if name.startswith('_'):
-            return super().__getattribute__(name)
+            return super(Request, self).__getattribute__(name)
         url = 'https://{s._fmt}.smappi.org/{s._path}/{func}'.format(s=self, func=name)
         def wrap(*args, **kwargs):
             if args:
